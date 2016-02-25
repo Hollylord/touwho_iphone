ef3bf0024794fca0b869b10381aa9f5862bf3672
Hollylord <283228138@qq.com>
2016-02-25 17:20:07 +0800

    新增xxnib桥接类库，来实现首页项目分类的自定义选择按钮。

---
+++ classes/首页/views/WDProgramSelectView.h
@@ -0,0 +1,14 @@
+//
+//  WDProgramSelectView.h
+//  touwhoIphone
+//
+//  Created by apple on 16/2/25.
+//  Copyright © 2016年 投壶. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XXNibBridge.h>
+
+@interface WDProgramSelectView : UIView <XXNibBridge>
+
+@end
---
+++ classes/首页/views/WDProgramSelectView.m
@@ -0,0 +1,21 @@
+//
+//  WDProgramSelectView.m
+//  touwhoIphone
+//
+//  Created by apple on 16/2/25.
+//  Copyright © 2016年 投壶. All rights reserved.
+//
+
+#import "WDProgramSelectView.h"
+
+@implementation WDProgramSelectView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
